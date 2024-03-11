766397255ba26d847021f6e1e0b44b991773ab26
Akaki Mikaia <akakimikaia@gmail.com>
2024-03-11 12:58:04 +0000

    feat: use tauri for building the app
    chore: add executable to lfs
    
    chore: track binaireis
    
    chore: testing new tauri setup
    
    chore: testing new tauri setup
    
    chore: testing new tauri setup
    
    feat: implement multi platform deployment using tauri

---
+++ src-backend/server.py
@@ -0,0 +1,112 @@
+
+# from io import BytesIO
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.responses import StreamingResponse
+# from PyPDF2 import PdfReader
+# import json
+# from transformers import AutoTokenizer, AutoModel
+# import torch
+# import torch.nn.functional as F
+# import spacy
+import multiprocessing
+import uvicorn
+
+# nlp = spacy.load("en_core_web_sm")
+# # Load model from HuggingFace Hub
+# tokenizer = AutoTokenizer.from_pretrained(
+#     'sentence-transformers/all-MiniLM-L6-v2')
+# model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+
+
+# # Mean Pooling - Take attention mask into account for correct averaging
+# def mean_pooling(model_output, attention_mask):
+#     # First element of model_output contains all token embeddings
+#     token_embeddings = model_output[0]
+#     input_mask_expanded = attention_mask.unsqueeze(
+#         -1).expand(token_embeddings.size()).float()
+#     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+
+# def encoder(sentences):
+#     encoded_input = tokenizer(
+#         sentences, padding=True, truncation=True, return_tensors='pt')
+#     # Compute token embeddings
+#     with torch.no_grad():
+#         model_output = model(**encoded_input)
+
+#     # Perform pooling
+#     sentence_embeddings = mean_pooling(
+#         model_output, encoded_input['attention_mask'])
+
+#     # Normalize embeddings
+#     sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
+
+#     return sentence_embeddings
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# @app.get("/tokenise-sentence")
+# def tokeniseSentence(q: str):
+#     sentence_embeddings = encoder([q])
+#     return {"result": sentence_embeddings.tolist()}
+
+
+# async def iterFile(file):
+#     text = await file.read()
+#     file_like_object = BytesIO(text)
+#     reader = PdfReader(file_like_object)
+#     # details = []
+#     print(reader.pages)
+#     for page in reader.pages:
+#         page_content = page.extract_text()
+#         last_line = page_content.splitlines()[-1]
+#         page_number = -1
+#         # if last new line is a number then remove it and use it as page number
+#         if last_line.isdigit():
+#             page_number = int(last_line)
+#         doc = nlp(page_content)
+#         sentences = list(doc.sents)
+#         sentences = [str(sentence) for sentence in sentences]
+
+#         sentence_vectors = encoder(sentences)
+#         # details.append({"page": page_number, "text": sentences, "vectors": sentence_vectors.tolist()})
+#         yield str(json.dumps({"page": page_number}))
+
+
+# @app.post("/tokenise-text")
+# async def tokenise_text(file: UploadFile):
+#     # for each page return number of the page and the text
+#     return StreamingResponse(iterFile(file))
+# @app.get("/tokenise-text")
+# def tokenise_text(q: str):
+#     doc = nlp(q)
+#     sentences = list(doc.sents)
+#     sentences = [str(sentence) for sentence in sentences]
+
+#     sentence_vectors = encoder(sentences)
+#     return {"result": sentence_vectors.tolist()}
+
+@app.get("/ping")
+def splitSentences():
+    return {"result": "pog"}
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()  # For Windows support
+    uvicorn.run(app, host="0.0.0.0", port=8135, reload=False, workers=1)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	// import { invoke } from '@tauri-apps/api/tauri';
+
+	import { open } from '@tauri-apps/api/dialog';
+	// import { Command } from '@tauri-apps/api/shell';
+	// alternatively, use `window.__TAURI__.shell.Command`
+	// `binaries/my-sidecar` is the EXACT value specified on `tauri.conf.json > tauri > bundle > externalBin`
+	// we need to cut posteriors otherwise it will be hard
+	let query = 'default query';
+	let response: { score: String; text: string }[] = [];
+	async function handleQuery() {}
+  
+  let error = 'no error';
+	async function handleUpload() {
+    const files = await open({
+      multiple: true
+		});
+
+		const fetchSentenceQuery = await fetch(
+			`http://localhost:8135/tokenise-sentence?q=${query}`
+		);
+		const result = await fetchSentenceQuery.json();
+    error = result
+    response.push({
+      score: '54',
+      text: result.result
+    });
+    response = [...response];
+	}
+</script>
+
+<div class="wrapper">
+	<div>
+		<label for="many">Upload multiple files of any type:</label>
+		<button on:click={handleUpload}>Upload</button>
+	</div>
+
+	<textarea placeholder="Type your query" bind:value={query} />
+	<button on:click={handleQuery}>Find</button>
+</div>
+{error}
+{#if response}
+	<div>
+		{#each response as { score, text }}
+			<div>
+				<p>{text}</p>
+				<p>{score}</p>
+			</div>
+		{/each}
+	</div>
+{/if}
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		height: 50vh;
+	}
+</style>
