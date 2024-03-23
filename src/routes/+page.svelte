0c75b65a41aafe1574c1cf41caecce73e33d7f84
Akaki Mikaia <akakimikaia@gmail.com>
2024-03-23 15:09:16 +0000

    feat: change pickle_file location

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { open } from '@tauri-apps/api/dialog';
-	import { fetch as rustFetch } from '@tauri-apps/api/http';
-	import { resourceDir, join as pathJoin } from '@tauri-apps/api/path';
+	// import { fetch as rustFetch } from '@tauri-apps/api/http';
+	import {  appLocalDataDir } from '@tauri-apps/api/path';
 
 	import Tree from '$lib/tree/Index.svelte';
 	import Separator from '$lib/separator/Separator.svelte';
@@ -13,22 +13,18 @@
 	let response: responseDetails[] = [];
 	let totalPages = 0;
 	let currentPage = 0;
-  let consoleMessage = 'This is a console message';
 	async function handleQuery() {
 		try {
-			const resourceDirPath = await resourceDir();
-      consoleMessage = resourceDirPath;
+			const appLocalDataDirPath = await appLocalDataDir();
 			const fetchSentenceQuery = await fetch(
-				`http://localhost:8135/find-similar-sentences?q=${query}&data_dir=${resourceDirPath}`
+				`http://localhost:8135/find-similar-sentences?q=${query}&data_dir=${appLocalDataDirPath}`
 			);
-      const res = await fetchSentenceQuery.json();
-      console.log(`🚀 ~ res:`, res);
-			const result = (res as { similar_sentences: responseDetails[]  })
-				.similar_sentences;
+			const res = await fetchSentenceQuery.json();
+			console.log(`🚀 ~ res:`, res);
+			const result = (res as { similar_sentences: responseDetails[] }).similar_sentences;
 			response = result;
 		} catch (e) {
 			console.log(`🚀 ~ e:`, e);
-      consoleMessage = e;
 		}
 	}
 
@@ -37,8 +33,8 @@
 		const files = (await open({
 			multiple: true
 		})) as string[];
-		const dataDirPath = await resourceDir();
-    query = dataDirPath;
+		const dataDirPath = await appLocalDataDir();
+		query = dataDirPath;
 		files &&
 			(await Promise.all(
 				files.map(async (filePath) => {
@@ -68,11 +64,20 @@
 				})
 			));
 	}
-	const treeItems = [{ title: 'index.svelte', icon: 'file' }];
+
+	const treeItems = [
+		{ title: 'index.svelte', icon: 'file' },
+		{ title: 'index.svelte', icon: 'file' },
+		{ title: 'index.svelte', icon: 'file' }
+	];
+
+	// add 20 more items to the tree
+	for (let i = 0; i < 20; i++) {
+		treeItems.push({ title: `index.svelte ${i}`, icon: 'file' });
+	}
 </script>
 
-{consoleMessage}
-<div class="flex h-[95vh]">
+<div class="flex h-[90vh]">
 	<div class="ml-[10px] flex flex-col justify-between">
 		<Tree {treeItems} />
 		<button class="rounded bg-magnum-500 px-4 py-2 font-bold" on:click={handleUpload}
@@ -110,7 +115,7 @@
 			{/if}
 		</div>
 		<div>
-      <p>{currentPage} out of {totalPages} processed</p>
+			<p>{currentPage} out of {totalPages} processed</p>
 			<Progress currentValue={currentPage} maxValue={totalPages} />
 		</div>
 	</div>
